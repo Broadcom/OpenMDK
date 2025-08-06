db9c678696800d1ebb8d331cb462acde85c31ffb
Broadcom SDK Release <sdk.releases@broadcom.com>
2025-08-06 15:49:32 -0700

    OpenMDK 2.10.9

---
+++ phy/include/phymod/phymod_util.h
@@ -0,0 +1,29 @@
+/*
+ *         
+ * 
+ *
+ * This software is governed by the Broadcom Switch APIs license.
+ * This license is set out in https://raw.githubusercontent.com/Broadcom-Network-Switching-Software/OpenMDK/master/Legal/LICENSE file.
+ * 
+ * Copyright 2007-2020 Broadcom Inc. All rights reserved.
+ *         
+ * Shell diagnostics of Phymod    
+ *
+ */
+
+#ifndef _PHYMOD_UTIL_H_
+#define _PHYMOD_UTIL_H_
+
+#include <phymod/phymod.h>
+
+
+/******************************************************************************
+Functions
+******************************************************************************/
+
+
+int phymod_util_lane_config_get(const phymod_access_t *phys, int *start_lane, int *num_of_lane);
+#define _SHR_LANEBMP_WBIT(_lane)                  (1U << (_lane))
+#define PHYMOD_LANEPBMP_MEMBER(_bmp, _lane)       (((_bmp) & (_SHR_LANEBMP_WBIT(_lane))) != 0)
+
+#endif /*_PHYMOD_UTIL_H_*/
